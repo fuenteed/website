7ac738211c9229ff2995d4c48420d34020e5771f
fuenteed <114704603+fuenteed@users.noreply.github.com>
2024-10-01 13:42:10 -0700

    fixed linkedin button on main page

--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -124,8 +124,28 @@ export default function Home() {
         {/* LinkedIn */}
         <Link href="https://www.linkedin.com/in/edson-fuentes-211001254/" target="_blank">
         <div class="social-button">
-
-        </div>
+    <button class="relative w-12 h-12 rounded-full group">
+      <div
+        class="floater w-full h-full absolute top-0 left-0 bg-blue-500 rounded-full duration-300 group-hover:-top-8 group-hover:shadow-2xl"
+      ></div>
+      <div
+        class="icon relative z-10 w-full h-full flex items-center justify-center border-2 border-blue-500 rounded-full"
+      >
+        <svg
+          height="24"
+          width="24"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            class="group-hover:fill-[#171543] fill-white duration-300"
+            d="M20,2H4C2.9,2,2,2.9,2,4v16c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V4C22,2.9,21.1,2,20,2z M8.5,19H6V10h2.5V19z M7.3,9 h-0.1C6.4,9,6,8.6,6,8.1V7.9c0-0.5,0.4-0.9,0.9-0.9h0.1C7.6,7,8,7.4,8,7.9v0.1C8,8.6,7.6,9,7.3,9z M19,19h-2.5v-4.9 c0-1.2-0.4-2-1.4-2c-0.8,0-1.3,0.6-1.5,1.2h-0.1V19H10V10h2.3v1.3h0C12.7,10.7,14,9.9,15.5,9.9c2.1,0,3.5,1.4,3.5,3.8V19z"
+            fill="#FFFFFF"
+          ></path>
+        </svg>
+      </div>
+    </button>
+  </div>
         </Link>
 
       </div>
