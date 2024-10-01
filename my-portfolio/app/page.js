649358a38e020ee6eca5ec8d1d1741b62dcd054f
fuenteed <114704603+fuenteed@users.noreply.github.com>
2024-10-01 13:36:10 -0700

    Updated main page

--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -134,23 +134,7 @@ export default function Home() {
       <div class="flex items-center gap-4 mt-4">
         <div>
           <Link href='/about'>
-          <button class="relative inline-flex items-center text-black text-lg font-normal transition ease-[cubic-bezier(0.25,0.8,0.25,1)] duration-[400ms] border-none bg-none hover:text-white focus:text-white">
-            <span>Více informací</span>
-            <span class="ml-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-                class="w-[0.9em] h-[0.8em] fill-black transform rotate-[-45deg] transition-transform duration-[500ms] ease-out hover:fill-white hover:rotate-0"
-              >
-                <path
-                  d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
-                ></path>
-              </svg>
-            </span>
-            <span
-              class="absolute bottom-[-7px] left-0 w-0 h-[1px] bg-black transition-all duration-[500ms] ease-[cubic-bezier(0.25,0.8,0.25,1)] hover:w-full"
-            ></span>
-          </button>
+         
             About
           </Link>
         </div>
